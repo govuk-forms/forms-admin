e7f9a5b08bf89a361f44a58db757e32e5971c996
Kelvin Gan <kelvin.gan@dsit.gov.uk>
2026-08-24 16:15:27 +0100

    Update feature spec to test removing domains

--- spec/features/organisations/manage_organisation_domains_spec.rb
+++ spec/features/organisations/manage_organisation_domains_spec.rb
@@ -9,6 +9,10 @@ describe "Add a new domain to an organisation" do
     and_i_add_a_domain
     and_i_visit_the_organisation_page
     then_i_see_the_new_domain_listed
+    and_there_is_a_remove_button_for_domains
+    when_i_click_on_the_remove_domain_button
+    then_i_confirm
+    i_no_longer_see_the_domain_for_the_organisation
   end
 
 private
@@ -32,5 +36,25 @@ private
 
   def then_i_see_the_new_domain_listed
     expect(page).to have_content "example.com"
+  end
+
+  def and_there_is_a_remove_button_for_domains
+    expect(page).to have_link "Remove"
+  end
+
+  def when_i_click_on_the_remove_domain_button
+    click_link "Remove"
+  end
+
+  def then_i_confirm
+    expect(page).to have_content "Are you sure you want to remove example.com from this organisation?"
+    choose "Yes"
+    click_button "Continue"
+    expect(page).to have_content "example.com has been removed from this organisation"
+  end
+
+  def i_no_longer_see_the_domain_for_the_organisation
+    visit organisation_path(organisation)
+    expect(page).not_to have_content "example.com"
   end
 end
