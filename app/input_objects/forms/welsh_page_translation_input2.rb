52818dbf2c4ee37c5fd77b4ba8343710cd037702
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2026-08-25 09:43:31 +0100

    Add alternate Welsh translation input objects
    These alternate input objects will be used when the feature flag is on,
    as they refer to exit pages rather than conditions. It will be easier to
    swap these in fully once the feature flag is being removed.

---
+++ app/input_objects/forms/welsh_page_translation_input2.rb
@@ -0,0 +1,276 @@
+class Forms::WelshPageTranslationInput2 < BaseInput
+  include TextInputHelper
+  include ActionView::Helpers::FormTagHelper
+  include ActiveModel::Attributes
+
+  attr_accessor :exit_page_translations, :selection_options_cy
+  attr_reader :page
+
+  attribute :id
+  attribute :question_text_cy
+  attribute :hint_text_cy
+  attribute :page_heading_cy
+  attribute :guidance_markdown_cy
+  attribute :none_of_the_above_question_cy
+
+  validate :question_text_cy_present?, on: :mark_complete
+  validate :question_text_cy_length, if: -> { question_text_cy.present? }
+
+  validate :hint_text_cy_present?, on: :mark_complete
+  validate :hint_text_cy_length, if: -> { hint_text_cy.present? }
+
+  validate :page_heading_cy_present?, on: :mark_complete
+  validate :page_heading_cy_length, if: -> { page_heading_cy.present? }
+
+  validate :guidance_markdown_cy_present?, on: :mark_complete
+  validate :guidance_markdown_cy_length_and_tags, if: -> { guidance_markdown_cy.present? }
+
+  validate :none_of_the_above_question_cy_present?, on: :mark_complete
+  validate :none_of_the_above_question_cy_length, if: -> { none_of_the_above_question_cy.present? }
+
+  validate :exit_page_translations_valid?
+  validate :selection_options_valid?
+
+  def initialize(attributes = {})
+    @page = attributes.delete(:page) if attributes.key?(:page)
+    super
+    self.id ||= @page&.id
+    @exit_page_translations ||= []
+    @selection_options_cy ||= []
+  end
+
+  def submit
+    return false if invalid?
+
+    page.question_text_cy = question_text_cy
+    page.hint_text_cy = page_has_hint_text? ? hint_text_cy : nil
+    page.page_heading_cy = page_has_page_heading_and_guidance_markdown? ? page_heading_cy : nil
+    page.guidance_markdown_cy = page_has_page_heading_and_guidance_markdown? ? guidance_markdown_cy : nil
+
+    exit_page_translations.presence&.each(&:submit)
+
+    page.answer_settings_cy = welsh_answer_settings
+
+    if page_has_none_of_the_above_question?
+      page.answer_settings_cy.none_of_the_above_question.question_text = none_of_the_above_question_cy
+    end
+
+    if page_has_selection_options?
+      page.answer_settings_cy.selection_options = DataStructType.new.cast_value(selection_options_cy.map(&:as_selection_option))
+    end
+
+    page.save!
+  end
+
+  def assign_page_values
+    return self unless page
+
+    self.question_text_cy = page.question_text_cy
+    self.hint_text_cy = page.hint_text_cy
+    self.page_heading_cy = page.page_heading_cy
+    self.guidance_markdown_cy = page.guidance_markdown_cy
+    self.none_of_the_above_question_cy = page.answer_settings_cy&.none_of_the_above_question&.question_text
+
+    exit_page_positions = ExitPage.positions_for_page(page)
+
+    self.exit_page_translations = page.exit_pages.map do |exit_page|
+      Forms::WelshExitPageTranslationInput.new(exit_page:, position: exit_page_positions[exit_page.id]).assign_exit_page_values
+    end
+
+    self.selection_options_cy = welsh_answer_settings&.selection_options&.map&.with_index do |selection_option, index|
+      Forms::WelshSelectionOptionTranslationInput.new(selection_option:, page:, id: index).assign_selection_option_values
+    end
+
+    self
+  end
+
+  def exit_page_translations_attributes=(attributes)
+    submitted_exit_page_ids = attributes.values.map { |attrs| attrs["id"] }.compact
+
+    exit_pages_by_id = page.exit_pages.where(id: submitted_exit_page_ids).index_by(&:id)
+    exit_page_positions = ExitPage.positions_for_page(page)
+
+    self.exit_page_translations = attributes.values.map { |exit_page_attrs|
+      exit_page_id = exit_page_attrs["id"].to_i
+      exit_page_position = exit_page_positions[exit_page_id]
+      exit_page_object = exit_pages_by_id[exit_page_id]
+
+      # skip the exit page if it doesn't belong to the page
+      next unless exit_page_object
+
+      Forms::WelshExitPageTranslationInput.new(exit_page_attrs.symbolize_keys.merge(exit_page: exit_page_object, position: exit_page_position))
+    }.compact
+  end
+
+  def selection_options_cy_attributes=(attributes)
+    self.selection_options_cy = attributes.values.map { |selection_option_attrs|
+      # Get the English selection option from the page's answer settings
+      selection_option_index = selection_option_attrs["id"].to_i
+      english_selection_option = page.answer_settings&.selection_options&.[](selection_option_index)
+
+      # Skip if the selection option doesn't exist in the English version
+      next unless english_selection_option
+
+      Forms::WelshSelectionOptionTranslationInput.new(**selection_option_attrs.symbolize_keys, page:, selection_option: english_selection_option)
+    }.compact
+  end
+
+  def form_field_id(attribute)
+    field_id(:forms_welsh_page_translation_input, page.id, :page_translations, attribute)
+  end
+
+  def page_has_hint_text?
+    page.hint_text.present?
+  end
+
+  def page_has_page_heading_and_guidance_markdown?
+    page.page_heading.present? && page.guidance_markdown.present?
+  end
+
+  def page_has_none_of_the_above_question?
+    return false unless page.answer_type == "selection"
+
+    page.answer_settings&.none_of_the_above_question&.question_text.present?
+  end
+
+  def question_text_cy_present?
+    if question_text_cy.blank?
+      errors.add(:question_text_cy, :blank, question_number: page.position, url: "##{form_field_id(:question_text_cy)}")
+    end
+  end
+
+  def question_text_cy_length
+    return if question_text_cy.length <= QuestionTextValidation::QUESTION_TEXT_MAX_LENGTH
+
+    errors.add(:question_text_cy, :too_long, question_number: page.position, count: QuestionTextValidation::QUESTION_TEXT_MAX_LENGTH, url: "##{form_field_id(:question_text_cy)}")
+  end
+
+  def hint_text_cy_present?
+    if page_has_hint_text? && hint_text_cy.blank?
+      errors.add(:hint_text_cy, :blank, question_number: page.position, url: "##{form_field_id(:hint_text_cy)}")
+    end
+  end
+
+  def hint_text_cy_length
+    return if hint_text_cy.length <= 500
+
+    errors.add(:hint_text_cy, :too_long, question_number: page.position, count: 500, url: "##{form_field_id(:hint_text_cy)}")
+  end
+
+  def page_heading_cy_present?
+    if page_has_page_heading_and_guidance_markdown? && page_heading_cy.blank?
+      errors.add(:page_heading_cy, :blank, question_number: page.position, url: "##{form_field_id(:page_heading_cy)}")
+    end
+  end
+
+  def page_heading_cy_length
+    return if page_heading_cy.length <= 250
+
+    errors.add(:page_heading_cy, :too_long, question_number: page.position, count: 250, url: "##{form_field_id(:page_heading_cy)}")
+  end
+
+  def guidance_markdown_cy_present?
+    if page_has_page_heading_and_guidance_markdown? && guidance_markdown_cy.blank?
+      errors.add(:guidance_markdown_cy, :blank, question_number: page.position, url: "##{form_field_id(:guidance_markdown_cy)}")
+    end
+  end
+
+  def guidance_markdown_cy_length_and_tags
+    markdown_validation = GovukFormsMarkdown.validate(guidance_markdown_cy)
+
+    return true if markdown_validation[:errors].empty?
+
+    if markdown_validation[:errors].include?(:too_long)
+      errors.add(:guidance_markdown_cy, :too_long, count: "4,999", question_number: page.position, url: "##{form_field_id(:guidance_markdown_cy)}")
+    end
+
+    tag_errors = markdown_validation[:errors].excluding(:too_long)
+    if tag_errors.any?
+      errors.add(:guidance_markdown_cy, :unsupported_markdown_syntax, question_number: page.position, url: "##{form_field_id(:guidance_markdown_cy)}")
+    end
+  end
+
+  def none_of_the_above_question_cy_present?
+    if page_has_none_of_the_above_question? && none_of_the_above_question_cy.blank?
+      errors.add(:none_of_the_above_question_cy, :blank, question_number: page.position, url: "##{form_field_id(:none_of_the_above_question_cy)}")
+    end
+  end
+
+  def none_of_the_above_question_cy_length
+    return if none_of_the_above_question_cy.length <= 250
+
+    errors.add(:none_of_the_above_question_cy, :too_long, count: 250, question_number: page.position, url: "##{form_field_id(:none_of_the_above_question_cy)}")
+  end
+
+  def form_marked_complete?
+    mark_complete == "true"
+  end
+
+  def exit_page_translations_valid?
+    return if exit_page_translations.nil?
+
+    exit_page_translations.each do |exit_page_translation|
+      exit_page_translation.validate(validation_context)
+
+      errors.merge!(exit_page_translation.errors)
+    end
+  end
+
+  def selection_options_valid?
+    return if selection_options_cy.nil?
+
+    # We check for duplicates here rather than in the
+    # selection_option_translation because here we can see all the selection options at once
+
+    # This is a list of all the selection options, which we'll use to check for duplicates
+    selection_options_without_blanks = selection_options_cy.map(&:name_cy).compact_blank
+
+    # If there are any duplicates, add a single error and link it to the first selection option
+    if selection_options_without_blanks.uniq.count != selection_options_without_blanks.count
+      first_selection_option_id = selection_options_cy.first.form_field_id(:name_cy)
+      errors.add(:selection_options_cy, :uniqueness, duplicate: selection_options_without_blanks.first, question_number: page.position, url: "##{first_selection_option_id}")
+    end
+
+    # Import all the errors from the child objects
+    selection_options_cy.each do |selection_option_translation|
+      selection_option_translation.validate(validation_context)
+
+      selection_option_translation.errors.each do |error|
+        errors.import(error, { attribute: "select_option_#{selection_option_translation.id}_#{error.attribute}".to_sym })
+      end
+    end
+  end
+
+  def page_has_selection_options?
+    page.answer_type == "selection"
+  end
+
+  # We need to normalize the Welsh answer settings to match the English ones.
+  # The only answer settings that need translating are the selection options
+  # We ensure that welsh answer settings are correct by copying the English
+  # ones setting the selection_options using any existing translations we have
+  # and emptying any we don't have.
+  def welsh_answer_settings
+    return nil unless page_has_selection_options?
+
+    answer_settings_cloned = DataStructType.new.cast_value(page.answer_settings.as_json)
+
+    answer_settings_cloned.selection_options.each.with_index do |selection_option, index|
+      selection_option.name = page.answer_settings_cy&.dig("selection_options", index, "name") || ""
+    end
+
+    if answer_settings_cloned.none_of_the_above_question.present?
+      answer_settings_cloned.none_of_the_above_question.question_text = page.answer_settings_cy&.none_of_the_above_question&.question_text || ""
+    end
+
+    answer_settings_cloned
+  end
+
+  def all_fields_empty?
+    attributes.except!("id").values.all?(&:blank?)
+  end
+
+  def blanked?
+    all_fields_empty? && exit_page_translations.all?(&:all_fields_empty?) && selection_options_cy.all?(&:all_fields_empty?)
+  end
+end
---
+++ app/input_objects/forms/welsh_translation_input2.rb
@@ -0,0 +1,180 @@
+class Forms::WelshTranslationInput2 < Forms::MarkCompleteInput
+  include TextInputHelper
+  include ActiveModel::Attributes
+
+  attr_accessor :form, :page_translations
+
+  attribute :mark_complete
+
+  attribute :name_cy
+  attribute :privacy_policy_url_cy
+  attribute :support_email_cy
+  attribute :support_phone_cy
+  attribute :support_url_cy
+  attribute :support_url_text_cy
+  attribute :declaration_markdown_cy
+  attribute :what_happens_next_markdown_cy
+  attribute :payment_url_cy
+
+  validates :name_cy, presence: true, if: -> { marked_complete? }
+  validates :name_cy, length: { maximum: 500 }, if: -> { name_cy.present? }
+
+  validates :privacy_policy_url_cy, presence: true, if: -> { marked_complete? && form.privacy_policy_url.present? }
+  validates :privacy_policy_url_cy, url: true, if: -> { privacy_policy_url_cy.present? }
+  validates :privacy_policy_url_cy, exclusion: { in: %w[https://www.gov.uk/help/privacy-notice] }, if: -> { privacy_policy_url_cy.present? }
+
+  validates :support_email_cy, presence: true, if: -> { marked_complete? && form_has_support_email? }
+  validates :support_email_cy, email_address: true, allowed_email_domain: true, if: -> { support_email_cy.present? }
+
+  validates :support_phone_cy, presence: true, if: -> { marked_complete? && form_has_support_phone? }
+  validates :support_phone_cy, length: { maximum: 500 }, if: -> { support_phone_cy.present? }
+
+  validates :support_url_cy, presence: true, if: -> { marked_complete? && form_has_support_url? }
+  validates :support_url_cy, url: true, length: { maximum: 120 }, if: -> { support_url_cy.present? }
+
+  validates :support_url_text_cy, presence: true, if: -> { marked_complete? && form_has_support_url? }
+  validates :support_url_text_cy, length: { maximum: 120 }, if: -> { support_url_text_cy.present? }
+
+  validates :declaration_markdown_cy, presence: true, if: -> { marked_complete? && form_has_declaration? }
+  validates :declaration_markdown_cy, length: { maximum: 2000 }, if: -> { declaration_markdown_cy.present? }
+
+  validates :what_happens_next_markdown_cy, presence: true, if: -> { marked_complete? && form.what_happens_next_markdown.present? }
+  validates :what_happens_next_markdown_cy, markdown: { allow_headings: false }, if: -> { what_happens_next_markdown_cy.present? }
+
+  validates :payment_url_cy, presence: true, if: -> { marked_complete? && form_has_payment_url? }
+  validates :payment_url_cy, payment_link: true, if: -> { payment_url_cy.present? }
+
+  validate :page_translations_valid
+
+  def initialize(attributes = {})
+    @form = attributes.delete(:form)
+    super
+    @page_translations ||= []
+  end
+
+  def page_translations_attributes=(attributes)
+    # Get all submitted page IDs from the params hash.
+    submitted_page_ids = attributes.values.map { |attrs| attrs["id"] }.compact
+
+    # lookup hash for efficiency
+    pages_by_id = form.pages.where(id: submitted_page_ids).includes(:exit_pages).index_by(&:id)
+
+    self.page_translations = attributes.values.map { |page_attrs|
+      page_id = page_attrs["id"].to_i
+      page_object = pages_by_id[page_id]
+
+      # skip the page if it doesn't belong to the form
+      next unless page_object
+
+      Forms::WelshPageTranslationInput2.new(page_attrs.symbolize_keys.merge(page: page_object))
+    }.compact
+  end
+
+  def submit
+    return false if invalid?
+
+    form.name_cy = name_cy
+    form.declaration_markdown_cy = form_has_declaration? ? declaration_markdown_cy : nil
+    form.payment_url_cy = form_has_payment_url? ? payment_url_cy : nil
+    form.privacy_policy_url_cy = privacy_policy_url_cy
+    form.support_email_cy = form_has_support_email? ? support_email_cy : nil
+    form.support_phone_cy = form_has_support_phone? ? support_phone_cy : nil
+
+    if form_has_support_url?
+      form.support_url_cy = support_url_cy
+      form.support_url_text_cy = support_url_text_cy
+    else
+      form.support_url_cy = nil
+      form.support_url_text_cy = nil
+    end
+
+    form.what_happens_next_markdown_cy = what_happens_next_markdown_cy
+
+    page_translations.presence&.each(&:submit)
+
+    form.welsh_completed = mark_complete
+
+    # We add :cy to the available languages so that the welsh form can be
+    # previewed. This means that if mark_complete is false, welsh form docs
+    # will be generated.
+    form.available_languages = %w[en cy]
+
+    form.save_draft!
+  end
+
+  def assign_form_values
+    self.name_cy = form.name_cy
+    self.privacy_policy_url_cy = form.privacy_policy_url_cy
+    self.support_email_cy = form.support_email_cy
+    self.support_phone_cy = form.support_phone_cy
+    self.support_url_cy = form.support_url_cy
+    self.support_url_text_cy = form.support_url_text_cy
+    self.declaration_markdown_cy = form.declaration_markdown_cy
+    self.what_happens_next_markdown_cy = form.what_happens_next_markdown_cy
+    self.payment_url_cy = form.payment_url_cy
+
+    self.mark_complete = form.try(:welsh_completed)
+
+    self.page_translations = form.pages.map do |page|
+      Forms::WelshPageTranslationInput2.new(page:).assign_page_values
+    end
+
+    self
+  end
+
+  def blanked?
+    all_fields_empty? && page_translations.all?(&:blanked?)
+  end
+
+  def all_fields_empty?
+    attributes.except!("mark_complete").values.all?(&:blank?)
+  end
+
+  def form_has_declaration?
+    form.declaration_markdown.present?
+  end
+
+  def form_has_payment_url?
+    form.payment_url.present?
+  end
+
+  def form_has_support_url?
+    form.support_url.present?
+  end
+
+  def form_has_support_phone?
+    form.support_phone.present?
+  end
+
+  def form_has_support_email?
+    form.support_email.present?
+  end
+
+  def form_has_support_information?
+    form_has_support_email? || form_has_support_phone? || form_has_support_url?
+  end
+
+  def form_has_what_happens_next?
+    form.what_happens_next_markdown.present?
+  end
+
+  def form_has_privacy_information?
+    form.privacy_policy_url.present?
+  end
+
+  def page_translations_valid
+    return if page_translations.nil?
+
+    # We pass :mark_complete as the validation context so the page_translations
+    # can validate differently depending on whether the form is marked complete
+    validation_context = marked_complete? ? :mark_complete : nil
+
+    page_translations.each do |page_translation|
+      page_translation.validate(validation_context)
+
+      page_translation.errors.each do |error|
+        errors.import(error, { attribute: "page_#{page_translation.page.position}_#{error.attribute}".to_sym })
+      end
+    end
+  end
+end
